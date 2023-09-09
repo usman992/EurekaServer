c252946f395bc2df05ac6380f5c7cc6fa7c7b839
usman992 <usmanmohammah@gmail.com>
2023-09-09 01:32:22 -0400

    Initial commit

---
+++ src/main/java/com/example/eurekaTest/EurekaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/eurekaTest/EurekaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
